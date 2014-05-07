2f68fdde6b4544724a8964d54dcd66c63e2e06a3
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2014-05-07 23:32:29 +0300

    Initial commit.
    - Fetching works
    - CTRL-C exits nicely, no stack trace
    - Parser done, though a little bit of love wouldn't hurt

---
+++ russiannames.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import requests
+import sys
+
+verbose = 0
+
+
+def parseRuLang(data):
+    findable = 'xml:lang="ru">'
+
+    # Begin with <span lang
+    b = data.find(findable)
+    if b == -1:
+        return None
+    # Strip xml scrab out
+    tmp = data.find(">", b)
+    if tmp != -1:
+        b = tmp + 1
+
+    # ..until the closing </span>
+    e = data.find("</span>", b)
+    if e == -1:
+        return None
+
+    runame = data[b:e]
+    if runame.find(",") > 0:
+        runame = runame.split(",")[0]
+    if runame.find("(") > 0:
+        runame = data[b:e].split(" (")[0]
+    return runame
+
+def main():
+    listofnames = list()
+    with open("listofrussian.txt") as f:
+        listofnames = f.readlines()
+
+    for name in listofnames:
+        name = name.rstrip("\n")
+        wikipage = u"http://en.wikipedia.org/wiki/%s" % name
+
+        r = requests.get(wikipage)
+        if r == None:
+            print "Cannot find Russian language section for %s" % name.replace("_", " ")
+            continue
+
+        if verbose:
+            print wikipage
+
+        runame = parseRuLang(r.content)
+        print "%s = %s" % (name.replace("_", " "), runame)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "\nCTRL-C pressed"
+
