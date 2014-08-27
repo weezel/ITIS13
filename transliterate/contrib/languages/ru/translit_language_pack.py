a97f4cb3664bbe05e4e1cde070c0e7710be03a59
Toni <tonimik@gmail.com>
2014-08-27 15:50:45 +0300

    korjattu Х vastaan Passport standardia

--- transliterate/contrib/languages/ru/translit_language_pack.py
+++ transliterate/contrib/languages/ru/translit_language_pack.py
@@ -16,14 +16,15 @@ class RussianLanguagePack(TranslitLanguagePack):
     language_name = "Russian"
     character_ranges = ((0x0400, 0x04FF), (0x0500, 0x052F))
     mapping = (
-        u"abvgdezijklmnoprstufh'y'ABVGDEZIJKLMNOPRSTUFH'Y'",
-        u"абвгдезийклмнопрстуфхъыьАБВГДЕЗИЙКЛМНОПРСТУФХЪЫЬ",
+        u"abvgdezijklmnoprstuf'y'ABVGDEZIJKLMNOPRSTUF'Y'",
+        u"абвгдезийклмнопрстуфъыьАБВГДЕЗИЙКЛМНОПРСТУФЪЫЬ",
     )
     reversed_specific_mapping = (
         u"ёэЁЭъьЪЬ",
         u"eeEE''''"
     )
     pre_processor_mapping = {
+    	u"kh": u"х",
         u"zh": u"ж",
         u"ts": u"ц",
         u"ch": u"ч",
@@ -31,6 +32,7 @@ class RussianLanguagePack(TranslitLanguagePack):
         u"shch": u"щ",
         u"iu": u"ю",
         u"ia": u"я",
+        u"Kh": u"Х",
         u"Zh": u"Ж",
         u"Ts": u"Ц",
         u"Ch": u"Ч",
